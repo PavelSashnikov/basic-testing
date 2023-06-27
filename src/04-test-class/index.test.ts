31c914f5e43c37db998b02927a5cec65b43324d9
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-27 11:35:43 +0300

    feat: class testing (part)

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,37 +1,67 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  getBankAccount,
+  InsufficientFundsError,
+  TransferFailedError,
+} from '.';
 
 describe('BankAccount', () => {
+  const balance = 1212;
+  let acc: BankAccount;
+  let accForTranfer: BankAccount;
+
+  beforeEach(() => {
+    acc = getBankAccount(balance);
+    accForTranfer = getBankAccount(balance);
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(acc.getBalance()).toBe(balance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => {
+      acc.withdraw(balance + 1);
+    }).toThrow(InsufficientFundsError);
   });
 
   test('should throw TransferFailedError error when transferring more than balance', () => {
-    // Write your test here
+    expect(() => {
+      acc.transfer(balance + 1, accForTranfer);
+    }).toThrow(TransferFailedError);
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => {
+      acc.transfer(balance, acc);
+    }).toThrow();
+    // TransferFailedError if description will be changed
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const depSum = 10;
+    acc.deposit(depSum);
+
+    expect(acc.getBalance()).toBe(balance + depSum);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const wdSum = 10;
+    acc.withdraw(wdSum);
+
+    expect(acc.getBalance()).toBe(balance - wdSum);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const amount = balance / 10;
+    acc.transfer(amount, accForTranfer);
+
+    expect(acc.getBalance()).toBe(balance - amount);
+    expect(accForTranfer.getBalance()).toBe(balance + amount);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    expect.assertions(1);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
